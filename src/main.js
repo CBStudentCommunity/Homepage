b0226d4dd4d2e1cf1b9fd6c72b8fd76b0f44eead
Gecotron <gecotron@gmail.com>
2022-04-06 13:40:36 -0400

    Initial File commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+function rollover() {
+	document.getElementById("rollImg").src='images/cssLogo.png'
+
+}
+function rollback() {
+	document.getElementById("rollImg").src='images/htmlLogo.png'
+
+}
+function openmenu() {
+  document.getElementById("menubar").style.width = "200px";
+	//document.getElementById("openbtn").style.opacity = "0";
+}
+function closemenu() {
+  document.getElementById("menubar").style.width = "0";
+	//document.getElementById("openbtn").style.opacity = "1";
+}
