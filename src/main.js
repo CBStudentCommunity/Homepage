7aca985b01fcfbdebe611747febe489671682f4c
Michael Ward <gecotron@gmail.com>
2023-04-12 13:58:41 -0400

    More Responsiveness Testing

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ function mobileCSS() {
     ("ontouchstart" in document && navigator.userAgent.match(/Android/i)) ||
     navigator.userAgent.match(/iPhone/i)
   ) {
-    theme.setAttribute("href", "src/mobile.css");
+    theme.setAttribute("href", "/src/mobile.css");
   } else {
-    theme.setAttribute("href", "src/desktop.css");
+    theme.setAttribute("href", "/src/desktop.css");
   }
 }
